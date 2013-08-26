239272abc64fe97a5c5155e9eaa3f9801b3a0801
Yoshikawa Takuto <takutoyoshikawa@gmail.com>
2013-08-26 10:11:34 +0900

    Add tests

---
+++ spec/show_album_information_spec.js
@@ -0,0 +1,96 @@
+describe("Show album information.js", function() {
+  it("can get video id from simple nicovideos' url", function() {
+    var url = 'http://www.nicovideo.jp/watch/sm17654032'
+    var id = get_video_id(url)
+    expect(id).toBe('sm17654032');
+  });
+
+  it("can get video id from complex nicovideos' url", function() {
+    var url = 'http://www.nicovideo.jp/watch/sm17654032#hoge&foo=bar?param=foobar'
+    var id = get_video_id(url)
+    expect(id).toBe('sm17654032');
+  });
+
+  it("succeeds to get album information from vocadb", function() {
+    var flag = false
+    var json = load_album_information({
+      id: "sm17654032",
+      success: function(data){
+        flag = true
+      }
+    })
+    
+    waitsFor(
+      function(){ return flag; }, "The getting album information should be succeeded", 4000
+    )
+    runs(function(){
+      expect(flag).toBe(true);
+    });
+  });
+
+  it("fails to get album information from vocadb", function() {
+    var flag = false
+    var json = load_album_information({
+      id: "0",
+      error: function(textStatus, jqXHR){
+        flag = true
+      }
+    })
+    
+    waitsFor(
+      function(){ return flag; }, "The getting album information should be succeeded", 4000
+    )
+    runs(function(){
+      expect(flag).toBe(true);
+    });
+  });
+
+  it("can make album information as list", function() {
+    var json = [
+      {
+        AdditionalNames: "",
+        ArtistString: "Phasma feat. 初音ミク",
+        CreateDate: "2012-05-29T19:45:47",
+        DiscType: "Album",
+        Id: 1429,
+        Name: "P",
+        RatingAverage: 5,
+        RatingCount: 1,
+        ReleaseDate: {
+        Day: 29,
+        Formatted: "2012/04/29",
+        IsEmpty: false,
+        Month: 4,
+        Year: 2012
+        },
+        ReleaseEvent: "The Voc@loid M@ster 20",
+        Status: 1,
+        Version: 3
+      },
+      {
+        AdditionalNames: "Apollo",
+        ArtistString: "Various artists",
+        CreateDate: "2013-03-09T21:27:39",
+        DiscType: "Compilation",
+        Id: 2225,
+        Name: "アポロ",
+        RatingAverage: 0,
+        RatingCount: 0,
+        ReleaseDate: {
+        Day: 30,
+        Formatted: "2013/03/30",
+        IsEmpty: false,
+        Month: 3,
+        Year: 2013
+        },
+        ReleaseEvent: null,
+        Status: 1,
+        Version: 7
+      }
+    ]
+    var information = get_album_information(json).html()
+    
+    expect(information).toBe('<li><a target="_blank" href="http://vocadb.net/Album/Details/1429">"P"に収録</a></li><li><a target="_blank" href="http://vocadb.net/Album/Details/2225">"アポロ"に収録</a></li>');
+  });
+  
+});
---
+++ src/show_album_information.js
@@ -0,0 +1,73 @@
+var VOCADB_URL = 'http://vocadb.net'
+var VOCADB_SONB_API = VOCADB_URL + '/api/v1/Song/'
+var VOCADB_ALBUM_URL = VOCADB_URL + '/Album/Details/'
+var tags = get_all_tags()
+
+$('.videoDescription').after('<div id="vocalun_album_information"></div>')
+
+var old_description = $(".videoDescription").text()
+setInterval(function() {
+  if($(".videoDescription").text() != old_description){
+    show_album_information()
+    old_description = $(".videoDescription").text()
+  }
+}, 1000);
+
+function show_album_information(){
+  if(tags.indexOf("VOCALOID") != -1 || tags.indexOf("音楽") != -1){
+    var id = get_video_id(document.URL)
+    load_album_information({
+      success: function(json){
+        var dom = get_album_information(json['Albums'])
+        $('#vocalun_album_information').empty().append(dom)          
+      },
+      error: function(XMLHttpRequest){
+        console.log(XMLHttpRequest)
+      }
+    })
+  }
+}
+
+function load_album_information(param){
+  var url = VOCADB_SONB_API + 'ByPV?pvId=' +  param.id + '&service=NicoNicoDouga'
+  console.log(url)
+  $.ajax({
+    url: url,
+    cache: true,
+    crossDomain: true,
+    dataType: "jsonp",
+    success: function(data, textStatus, jqXHR){
+      if(data != null)
+        param.success(data, textStatus, jqXHR)
+      else
+        param.error(jqXHR, textStatus)
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      param.error(jqXHR, textStatus)
+    }
+  });
+}
+
+function get_all_tags(){
+  return $('.videoHeaderTagLink').map(function(i, val){
+    return val.text
+  }).get()
+}
+
+function get_video_id(url){
+  return url.split("/").slice(-1)[0].split("&")[0].split("#")[0]
+}
+
+function get_album_information(albums){
+  var album_list = $('<ul></ul>')
+  albums.forEach(function(album){
+    album_list.append('<li>' + 
+      '<a target="_blank" href="' + VOCADB_ALBUM_URL + album['Id'] + '">' +
+        '"' + album['Name'] + '"に収録' +
+      '</a>' +
+    '</li>' )
+  })
+  return album_list 
+}
+
+show_album_information()

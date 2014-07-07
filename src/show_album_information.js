a3c8829973ebde8eb55e81827ce69beeb164362d
Yoshikawa Takuto <takutoyoshikawa@gmail.com>
2014-07-08 03:27:23 +0900

    Support API v2. Add cache object.

--- src/show_album_information.js
+++ src/show_album_information.js
@@ -1,75 +1,177 @@
-var VOCADB_URL = 'http://vocadb.net'
-var VOCADB_SONB_API = VOCADB_URL + '/api/v1/Song/'
-var VOCADB_ALBUM_URL = VOCADB_URL + '/Album/Details/'
-var tags = get_all_tags()
-
-$('.videoDescription').after('<div id="vocalun_album_information"></div>')
-
-var old_description = $(".videoDescription").text()
-setInterval(function() {
-  if($(".videoDescription").text() != old_description){
-    show_album_information()
-    old_description = $(".videoDescription").text()
-  }
-}, 1000);
-
-function show_album_information(){
-  if(tags.indexOf("VOCALOID") != -1 || tags.indexOf("音楽") != -1){
-    var id = get_video_id(document.URL)
-    console.log(id)
-    load_album_information({
-      id: id,
-      success: function(json){
-        var dom = get_album_information(json['Albums'])
-        $('#vocalun_album_information').empty().append(dom)          
-      },
-      error: function(XMLHttpRequest){
-        console.log(XMLHttpRequest)
-      }
-    })
-  }
-}
-
-function load_album_information(param){
-  var url = VOCADB_SONB_API + 'ByPV?pvId=' +  param.id + '&service=NicoNicoDouga'
-  $.ajax({
-    url: url,
-    cache: true,
-    crossDomain: true,
-    jsonpCallback: "callback",
-    jsonp: "callback",
-    success: function(data, textStatus, jqXHR){
-      if(data != null)
-        param.success(data, textStatus, jqXHR)
-      else
-        param.error(jqXHR, textStatus)
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      param.error(jqXHR, textStatus)
+var Config = {
+    VocaDB: new function(){
+        this.PUBLIC_URL = 'http://vocadb.net/';
+        this.API_BASE_URL = this.PUBLIC_URL + 'api/';
+        this.ALBUM_PAGE_URL = this.PUBLIC_URL + 'Album/Details/';
     }
-  });
+};
+
+var VocalunConsole = new function(){
+    this.enable = false;
+    this.log = function(message) {
+        if(this.enable) {
+            console.log(message);
+        }
+    }
 }
 
-function get_all_tags(){
-  return $('.videoHeaderTagLink').map(function(i, val){
-    return val.text
-  }).get()
+var DomObserver = function(targetDom) {
+    this.onChanged = function(callback) {
+        var mo = new MutationObserver(function(mutationRecords){
+            callback();
+        });
+        mo.observe(
+            targetDom, { characterData: true, subtree: true }
+        );
+    }
+};
+
+var VocaDBFetcher = function(config) {
+    this.cache = new Cache(localStorage);
+
+    this.ONE_HOUR = 60 * 60 * 1000; //1 hour
+
+    this.getSongApiUrl = function(pvId){
+        return config.API_BASE_URL + "songs?pvService=NicoNicoDouga&pvId=" + pvId + "&fields=Albums";
+    }
+
+    this.fetchAlbumInformation = function(params) {
+      var url = this.getSongApiUrl(params.id);
+      VocalunConsole.log(url);
+
+      var cachedJson = this.cache.find(params.id);
+      if(cachedJson !== null) {
+        VocalunConsole.log("Cache used.")
+        params.success(cachedJson);
+        return;
+      }
+
+      $.ajax({
+        context: this,
+        headers: {
+            Accept : "text/json; charset=utf-8",
+            "Content-Type": "text/json; charset=utf-8"
+        },
+        url: url,
+        cache: true,
+        jsonpCallback: "callback",
+        jsonp: "callback",
+        success: function(data, textStatus, jqXHR){
+            if(data != null){
+            VocalunConsole.log("Cache stored.")
+                this.cache.store({
+                    key: params.id,
+                    value: data
+                }, this.ONE_HOUR);
+                params.success(data, textStatus, jqXHR)
+            } else {
+                params.error(jqXHR, textStatus)
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            params.error(jqXHR, textStatus, errorThrown)
+        }
+      });
+    }
+};
+
+var Cache = function(localStorage) {
+    this.store = function(keyValue, expireTimeInMilliSeconds) {
+        localStorage[keyValue.key] = JSON.stringify({
+            value: keyValue.value,
+            expireDate: Date.now() + expireTimeInMilliSeconds
+        });
+    }
+
+    this.find = function(key) {
+        if(localStorage[key] === undefined)
+            return null;
+
+        var valueAndExpireTime = JSON.parse(localStorage[key]);
+        if(Date.now() < valueAndExpireTime["expireDate"] && valueAndExpireTime !== null) {
+            return valueAndExpireTime.value;
+        } else {
+            delete(valueAndExpireTime);
+            return null;
+        }
+    }
 }
 
-function get_video_id(url){
-  return url.split("/").slice(-1)[0].split("?")[0].split("#")[0]
+var AlbumInformationRenderer = function(config) {
+    this.init = function() {
+        $('.videoDescription').after('<div id="vocalun_album_information"></div>');
+    }
+
+    this.render = function(albumJson) {
+        var dom = makeAlbumInformationDom(albumJson);
+        $('#vocalun_album_information').empty().append(dom);
+    }
+
+    this.reset = function(albumJson) {
+        $('#vocalun_album_information').empty();
+    }
+
+    function makeAlbumInformationDom(albumJson){
+        var album_list = $('<ul></ul>')
+        albumJson.forEach(function(album){
+            VocalunConsole.log(album);
+            album_list.append('<li>' +
+                '<a target="_blank" href="' + config.ALBUM_PAGE_URL + album['id'] + '">' +
+                '"' + album['name'] + '"に収録' +
+                '</a>' +
+                '</li>' )
+        })
+        return album_list
+    }
+};
+
+var VocalunController = function() {
+    var renderer = new AlbumInformationRenderer(Config.VocaDB);
+
+    this.main = function() {
+        renderer.init();
+        this.updateAlbumInformation();
+
+        var observer = new DomObserver($('#videoDetailInformation').get(0));
+        observer.onChanged(this.updateAlbumInformation);
+    }
+
+    this.updateAlbumInformation = function() {
+        renderer.reset();
+
+        var tags = getTags();
+        if(isTargetCategoryTag(tags)) {
+            var videoId = getVideoId(document.URL);
+
+            var fetcher = new VocaDBFetcher(Config.VocaDB);
+            fetcher.fetchAlbumInformation({
+                id: videoId,
+                success: function(json){
+                    if(json['albums']) {
+                        renderer.render(json['albums']);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    VocalunConsole.log(errorThrown);
+                }
+            });
+        };
+    }
+
+    function isTargetCategoryTag(tags) {
+        return tags.indexOf("VOCALOID") != -1 || tags.indexOf("音楽") != -1
+    }
+
+    function getTags(){
+        return $('.videoHeaderTagLink').map(function(i, val){
+            return val.text
+        }).get()
+    }
+
+    function getVideoId(url){
+        return url.split("/").slice(-1)[0].split("?")[0].split("#")[0]
+    };
 }
 
-function get_album_information(albums){
-  var album_list = $('<ul></ul>')
-  albums.forEach(function(album){
-    album_list.append('<li>' + 
-      '<a target="_blank" href="' + VOCADB_ALBUM_URL + album['Id'] + '">' +
-        '"' + album['Name'] + '"に収録' +
-      '</a>' +
-    '</li>' )
-  })
-  return album_list 
-}
-
-show_album_information()
+var controller = new VocalunController();
+controller.main();
